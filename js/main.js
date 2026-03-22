81a10c22df87dc4da0abb2c5ce933604a76ae9c9
Gaspar <bg154574@gmail.com>
2026-03-22 01:56:39 -0600

    mi pagina de flores 💖

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
+const text = "PARA MI LUNETA BOLUDA PRECIOSA 🥰❤️";
+let index = 0;
 
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-
-    const titles = ('I LOVE U').split('')
-    const titleElement = document.getElementById('title');
-    let index = 0;
-
-    function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
-      }
-    }
-
-    appendTitle();
-
-    clearTimeout(c);
-  }, 1000);
-};
+function appendTitle() {
+  if (index < text.length) {
+    titleElement.innerHTML = text.slice(0, index + 1);
+    index++;
+    setTimeout(appendTitle, 100);
+  }
+}
